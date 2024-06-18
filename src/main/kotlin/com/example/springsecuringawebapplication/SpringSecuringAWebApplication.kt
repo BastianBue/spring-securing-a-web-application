d77e96584592c0229aee6ce19df28ffe6b529af7
Bastian Büld <bastian.bueld@aoe.com>
2024-06-18 11:27:12 +0200

    feat: set up the application

---
+++ src/main/kotlin/com/example/springsecuringawebapplication/SpringSecuringAWebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsecuringawebapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecuringAWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecuringAWebApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springsecuringawebapplication/SpringSecuringAWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springsecuringawebapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecuringAWebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
